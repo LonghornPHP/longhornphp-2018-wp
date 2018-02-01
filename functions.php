d790b41730a2c2281d0a443bd620cd74f38980c2
Daniel Abernathy <dabernathy89@gmail.com>
2018-02-01 00:24:27 -0600

    display speaker meta

--- functions.php
+++ functions.php
@@ -116,7 +116,7 @@ add_action( 'widgets_init', 'base_theme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function base_theme_scripts() {
-	$version = '201801272';
+	$version = '20180131';
 	$url = get_site_url();
 
 	wp_enqueue_style( 'base_theme-style', get_template_directory_uri() . '/css/style.css', array(), $version );
--- template-parts/speaker-card.php
+++ template-parts/speaker-card.php
@@ -1,4 +1,4 @@
-<div class="card mb-4 rounded-0">
+<div class="speaker-card card mb-4 rounded-0" id="<?php echo esc_attr( $speaker->post_name ); ?>">
     <div class="card-body">
         <div class="row">
             <div class="col-12 <?php echo (empty($speaker->sessions) ? '' : 'col-lg-7'); ?>">
@@ -12,6 +12,24 @@
                 <?php if ($is_keynoter) : ?>
                     <h6>Keynote Speaker</h6>
                 <?php endif; ?>
+
+                <div class="meta">
+                    <?php if ($twitter = get_post_meta( $speaker->ID, 'speaker_twitter', true )) : ?>
+                        <p class="twitter-link">
+                            <i class="fab fa-twitter-square"></i>&nbsp;
+                            <a target="_blank" href="https://www.twitter.com/<?php echo esc_attr( $twitter ); ?>">
+                                @<?php echo esc_html( $twitter ); ?>
+                            </a>
+                        </p>
+                    <?php endif; ?>
+                    <?php if ($company = get_post_meta( $speaker->ID, 'speaker_company', true )) : ?>
+                        <p class="company">
+                            <i class="fas fa-briefcase"></i>
+                            &nbsp;<?php echo esc_html( $company ); ?>
+                        </p>
+                    <?php endif; ?>
+                </div>
+
                 <p class="card-text"><?php echo apply_filters('the_content', $speaker->post_content); ?></p>
             </div>
             <?php if (!empty($speaker->sessions)) : ?>
