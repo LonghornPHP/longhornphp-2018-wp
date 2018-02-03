ac8f6a1053ca0e874ba0b44869643eab6092c25a
Daniel Abernathy <dabernathy89@gmail.com>
2018-02-03 09:13:17 -0600

    add user images to session list

--- template-parts/session-card.php
+++ template-parts/session-card.php
@@ -1,18 +1,27 @@
 <li class="list-group-item session-card" id="<?php echo esc_attr( $session->post_name ); ?>">
     <div class="d-flex align-items-start collapsed" data-toggle="collapse" data-target="#<?php echo esc_attr( $session->post_name ); ?>-description" aria-expanded="false" aria-controls="<?php echo esc_attr( $session->post_name ); ?>-description">
-        <div class="toggle">
+        <!-- <div class="toggle">
             <i class="far fa-caret-square-right"></i>
-        </div>
+        </div> -->
 
         <div class="meta">
-            <h4><?php echo esc_html( $session->post_title ); ?></h4>
-            <?php if (count($session->speakers)) : ?>
-                <h5>
-                    <a href="<?php the_field('speakers_page', 'options'); ?>#<?php echo esc_attr( $session->speakers[0]->post_name ); ?>">
-                        <i class="fas fa-user"></i> <?php echo esc_html( $session->speakers[0]->post_title ); ?>
-                    </a>
-                </h5>
+            <?php $has_thumbnail = count($session->speakers) && has_post_thumbnail( $session->speakers[0]->ID ); ?>
+            <?php if ($has_thumbnail) : ?>
+                <div class="float-left img-wrap">
+                    <?php echo get_the_post_thumbnail( $session->speakers[0]->ID, 'thumbnail' ); ?>
+                </div>
             <?php endif; ?>
+
+            <div class="<?php echo $has_thumbnail ? 'title pl-3 float-right' : ''; ?>">
+                <h4><?php echo esc_html( $session->post_title ); ?></h4>
+                <?php if (count($session->speakers)) : ?>
+                    <h5>
+                        <a href="<?php the_field('speakers_page', 'options'); ?>#<?php echo esc_attr( $session->speakers[0]->post_name ); ?>">
+                            <?php echo esc_html( $session->speakers[0]->post_title ); ?>
+                        </a>
+                    </h5>
+                <?php endif; ?>
+            </div>
         </div>
 
         <?php $type = get_the_terms( $session->ID, 'session_type' ); ?>
