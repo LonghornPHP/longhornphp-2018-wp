bc92413f7f3732fb209d02b6ff01d1dcf56d78e3
Daniel Abernathy <dabernathy89@gmail.com>
2018-02-02 18:10:27 -0600

    home page style tweaks

--- page-templates/home.php
+++ page-templates/home.php
@@ -3,7 +3,7 @@
 get_header();
 ?>
 
-<div class="top-section" style="background-image: url('<?php echo get_the_post_thumbnail_url(get_the_ID(), 'large'); ?>">
+<div class="top-section">
 	<div class="container">
 		<div class="row justify-content-center align-items-center">
 			<div class="text-center text-sm-left col-sm-4 logo-wrap">
@@ -18,13 +18,6 @@ get_header();
 				<?php endwhile; ?>
 			</div>
 		</div>
-	</div>
-</div>
-
-<div class="section-register" id="register">
-	<div class="container">
-		<h2 class="text-center">Buy a Ticket</h2>
-		<?php the_field('home_registration_embed_code'); ?>
 	</div>
 </div>
 
@@ -73,6 +66,28 @@ get_header();
 
 <?php endif; ?>
 
+<div class="section-sponsors" id="sponsors">
+	<div class="container">
+		<div class="text-center mb-4">
+			<h2>Sponsors</h2>
+		</div>
+		<div class="sponsors-list">
+			<?php get_template_part( 'template-parts/sponsors' ); ?>
+
+			<div class="text-center">
+				<?php the_field('home_sponsors_intro'); ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="section-register" id="register" style="background-image: url('<?php echo get_the_post_thumbnail_url(get_the_ID(), 'large'); ?>">
+	<div class="container">
+		<h2 class="text-center">Buy a Ticket</h2>
+		<?php the_field('home_registration_embed_code'); ?>
+	</div>
+</div>
+
 <div
 	class="section-about"
 	style="background-image: url('<?php echo wp_get_attachment_image_url( get_field('home_description_image'), 'large' ); ?>');">
@@ -82,18 +97,6 @@ get_header();
 				<?php the_field('home_description_content'); ?>
 				<?php the_field('home_description_mailchimp_embed'); ?>
 			</div>
-		</div>
-	</div>
-</div>
-
-<div class="section-sponsors" id="sponsors">
-	<div class="container">
-		<div class="text-center mb-4">
-			<h2>Sponsors</h2>
-			<?php the_field('home_sponsors_intro'); ?>
-		</div>
-		<div class="sponsors-list">
-			<?php get_template_part( 'template-parts/sponsors' ); ?>
 		</div>
 	</div>
 </div>
